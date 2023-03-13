d94b21e3aa9cf7e417080b379689c68eb6a369c6
Vaibhav Thakur <vaibhavt802@gmail.com>
2023-03-13 23:16:19 +0530

    Initial Commit

---
+++ Hello World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello World
+//
+//  Created by Vaibhav Thakur on 09/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
